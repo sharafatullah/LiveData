73b0ebcd49fecb9ef006da450cf004b0302c4659
sharafatshaikh <sharafatshaikh7@gmail.com>
2020-06-20 12:49:10 +0530

    Initial commit

---
+++ app/src/main/java/com/shaikh/livedata/MainActivity.java
@@ -0,0 +1,52 @@
+package com.shaikh.livedata;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NameViewModel model;
+    TextView nameTextView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTextView = findViewById(R.id.nameTextView);
+        Button button = findViewById(R.id.btnChangeName);
+
+        // Get the ViewModel.
+        model = new ViewModelProvider(this).get(NameViewModel.class);
+
+        // Create the observer which updates the UI.
+        final Observer<String> nameObserver = new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String newName) {
+                // Update the UI, in this case, a TextView.
+                nameTextView.setText(newName);
+            }
+        };
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        model.getCurrentName().observe(this, nameObserver);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String anotherName = "Hello From Live Data Android";
+                model.getCurrentName().setValue(anotherName);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/shaikh/livedata/NameViewModel.java
@@ -0,0 +1,17 @@
+package com.shaikh.livedata;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NameViewModel extends ViewModel {
+
+    // Create a LiveData with a String
+    private MutableLiveData<String> currentName;
+
+    public MutableLiveData<String> getCurrentName() {
+        if (currentName == null) {
+            currentName = new MutableLiveData<String>();
+        }
+        return currentName;
+    }
+}
